1d3f1869a47ced5ff2178c704da1bbd178a2151c
dia-na21 <dianaandradee0@gmail.com>
2025-09-20 08:57:11 -0600

    operaciones

---
+++ src/app/formularios/operas-bas/operas-bas.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operas-bas',
+  standalone: false,
+  templateUrl: './operas-bas.component.html',
+  styleUrl: './operas-bas.component.css'
+})
+export class OperasBasComponent {
+
+num1:string="";
+num2:string="";
+res:string="";
+opera:string="";
+
+sumar():void{
+  this.res = (parseInt(this.num1) + parseInt(this.num2)).toString();
+
+}
+
+restar():void{
+  this.res = (parseInt(this.num1) - parseInt(this.num2)).toString();
+
+}
+
+multiplicar():void{
+  this.res = (parseInt(this.num1) * parseInt(this.num2)).toString();
+
+}
+
+dividir():void{
+  this.res = (parseInt(this.num1) / parseInt(this.num2)).toString();
+
+}
+calculos():void{
+switch (this.opera){
+  case 'Suma':
+        this.sumar();
+        break;
+      case 'Resta':
+        this.restar();
+        break;
+      case 'Multiplicar':
+        this.multiplicar();
+        break;
+      case 'Dividir':
+        this.dividir();
+        break;
+}
+
+}
+
+
+}
